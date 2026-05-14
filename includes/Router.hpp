5ab2769efa1c2e9a5c35f81a7824e633084b8630
aboumata <fanchkhow@gmail.com>
2026-05-14 18:50:24 +0100

    Add `match` method to `Router` class for URI-to-location matching

--- includes/Router.hpp
+++ includes/Router.hpp
@@ -1,4 +1,11 @@
 #ifndef WEBSERVER_ROUTER_HPP
 #define WEBSERVER_ROUTER_HPP
 
+#include "types.hpp"
+
+class Router {
+public:
+    const LocationConfig *match(const std::string& uri, const ServerConfig& server);
+};
+
 #endif
