a9b8391ccc2518f31f3dd8892c941d3296199315
Mohammed Chafiki <schafiki678@gmail.com>
2026-05-18 22:04:13 +0100

    test is working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "../includes/types.hpp"
 #include "../includes/Router.hpp"
+#include <iostream>
 
-int main (int ac, char **av) {
-    (void)ac;
-    (void)av;
+// int main (int ac, char **av) {
+//     (void)ac;
+//     (void)av;
+//     std::cout << "Line " << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
