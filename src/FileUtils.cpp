8a1e3afb4f555778d1e6fed464722f5056302de3
Aboumata <fanchkhow@gmail.com>
2026-07-21 00:26:10 +0100

    Add file utility methods for path access and file reading

--- src/FileUtils.cpp
+++ src/FileUtils.cpp
@@ -1,6 +1,9 @@
 #include "../includes/FileUtils.hpp"
+
+#include <fstream>
 #include "sys/stat.h"
 #include <sstream>
+#include <unistd.h>
 
 //"Where on MY disk does this URL actually point?"
 //"Does that file exist? Is it a folder? Am I allowed to read it?"
@@ -42,10 +45,43 @@ bool FileUtils::is_path_safe(const std::string& uri)
 
 bool FileUtils::file_exists(const std::string& path)
 {
-    
+    struct stat fileInfo = {};
+    if (stat(path.c_str(), &fileInfo) != 0)
+        return false;
+    return true;
 }
 
 bool FileUtils::is_directory(const std::string& path)
 {
+    struct stat fileInfo = {};
+    if (stat(path.c_str(), &fileInfo) != 0)
+        return false;
 
+    return S_ISDIR(fileInfo.st_mode) != 0;
+}
+
+bool FileUtils::is_readable(const std::string& path)
+{
+    if (access(path.c_str(), R_OK) != 0)
+        return false;
+    return true;
+}
+
+bool FileUtils::is_writable(const std::string& path)
+{
+    if (access(path.c_str(), W_OK) != 0)
+        return false;
+    return true;
+}
+
+bool FileUtils::read_file(const std::string& path, std::string& out)
+{
+    std::ifstream file(path.c_str(), std::ios::in | std::ios::binary);
+    if (!file.is_open())
+        return false;
+    std::ostringstream ss;
+    ss << file.rdbuf();
+    out = ss.str();
+
+    return true;
 }
