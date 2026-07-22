ae0003d2c743544ae4d30b24f404b5b0dd055b6b
Aboumata <fanchkhow@gmail.com>
2026-07-22 17:47:51 +0100

    Introduce `GetHandler` to process HTTP GET requests

--- includes/GetHandler.hpp
+++ includes/GetHandler.hpp
@@ -1,4 +1,14 @@
 #ifndef WEBSERVER_GETHANDLER_HPP
 #define WEBSERVER_GETHANDLER_HPP
 
+#include "types.hpp"
+
+class GetHandler {
+private:
+    GetHandler();
+    static HttpResponse make_response(int statusCode);
+public:
+    static HttpResponse handle(const HttpRequest& request, const LocationConfig& location);
+};
+
 #endif
---
+++ src/GetHandler.cpp
@@ -0,0 +1,43 @@
+#include "../includes/GetHandler.hpp"
+#include "../includes/FileUtils.hpp"
+
+HttpResponse GetHandler::make_response(int statusCode)
+{
+    HttpResponse response = {};
+    response.status_code = statusCode;
+    switch (statusCode)
+    {
+    case 200:
+        response.status_message = "OK";
+        break;
+    case 301:
+        response.status_message = "Moved Permanently";
+        break;
+    case 403:
+        response.status_message = "Forbidden";
+        break;
+    case 404:
+        response.status_message = "Not Found";
+        break;
+    case 500:
+        response.status_message = "Internal Server Error";
+        break;
+    default:
+        response.status_message = "Unknown Status";
+    }
+    return response;
+}
+
+HttpResponse GetHandler::handle(const HttpRequest& request, const LocationConfig& location)
+{
+    std::string diskPath;
+
+    if (!FileUtils::is_path_safe(request.uri))
+        return make_response(403);
+    if (!FileUtils::resolve_path(location.root, request.uri, diskPath))
+        return make_response(500);
+    if (!FileUtils::file_exists(request.uri))
+        return make_response(404);
+
+    return make_response(200);
+}
