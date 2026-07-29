e29054a3cc00da0e2b530239b67fc54926cba776
Aboumata <fanchkhow@gmail.com>
2026-07-29 17:02:45 +0100

    Add Dispatcher class with static `dispatch` method definition

--- includes/Dispatcher.hpp
+++ includes/Dispatcher.hpp
@@ -1,6 +1,12 @@
 #ifndef WEBSERVER_DISPATCHER_HPP
 #define WEBSERVER_DISPATCHER_HPP
+#include "types.hpp"
 
-
+class Dispatcher{
+private:
+    Dispatcher();
+public:
+    static HttpResponse dispatch(const HttpRequest& request, const ServerConfig& server);
+};
 
 #endif
